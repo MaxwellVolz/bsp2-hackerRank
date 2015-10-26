220830ec47eff8b265620fb81bb4efc76332a735
max <maxwellvolz@gmail.com>
2015-10-26 09:49:05 -0700

    fixed directions

--- bot.js
+++ bot.js
@@ -12,32 +12,35 @@ var test = function(input){
 	
 	for(x=2,k=rows.length-1;x<k;++x){
 		
-		
 		if(rows[x].indexOf('p') != -1){
 			var pY = x-2,
 				pX = rows[x].indexOf('p');
 			
 			if(mY < pY){
 				while(mY < pY){
-					console.log("UP");
+					console.log("DOWN");
+					return; //because 1 move at a time
 					++mY;
 				}
 			}
-			if(mY > pY){
+			else if(mY > pY){
 				while(mY > pY){
-					console.log("DOWN");
+					console.log("UP");
+					return; //because 1 move at a time
 					--mY;
 				}
 			}
 			if(mX < pX){
 				while(mX < pX){
 					console.log("RIGHT");
+					return; //because 1 move at a time
 					++mX;
 				}
 			}
-			if(mX > pX){
+			else if(mX > pX){
 				while(mX > pX){
 					console.log("LEFT");
+					return; //because 1 move at a time
 					--mX;
 				}
 			}
@@ -52,6 +55,6 @@ var test = function(input){
 
 
 
-var input = "5\n2 3\n-----\n-----\np--m-\n-----\n----\n";
+var input = "5\n1 2\n-----\n--m--\n-----\n-----\n-p---\n";
 
 test(input);
